f1307723bc9f7de904752446f2bd8408771c24cd
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-09-26 21:18:53 +0530

    Made the graphql structure

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const {} = require('graphql');
 
 const app = express()
 
 app.use(bodyParser);
+
+app.use('/graphql', graphqlHTTP({
+    schema: null,
+    rootValue: {}
+}))
 
 app.listen(3000);
